af63a88dd94fefc2bdad3d153882a92fc7059837
Sowparna Shome <soupornoshome@gmail.com>
2025-03-23 23:59:12 +0530

    create todo and db connection create

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,7 +1,8 @@
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../auth/[...nextauth]/authOptions";
 import Todo, { ITodo } from "@/models/todo.model";
+import { connectToDatabase } from "@/db/connectToDatabase";
 
 export async function GET() {
     try {
@@ -10,16 +11,46 @@ export async function GET() {
             return NextResponse.json({ error: "🚫 Unauthorized. Please log in to retrieve the todos." }, { status: 401 });
         }
         const userId = session?.user.id;
+        //const userId = "67e051d767ef5cc8fa0a5f83";
+        await connectToDatabase();
         const todos: ITodo[] = await Todo.find({ userId: userId });
         if (!todos) {
             return NextResponse.json({ error: "🚫 Failed to retrieve the todos." }, { status: 401 });
         }
-        return NextResponse.json({ todos: todos }, { status: 201 });
+        return NextResponse.json({ message:"✅ Successfully fetched the todos.",todos: todos }, { status: 201 });
         
     } catch (error) {
         console.error("❌ Error retrieving the todos:", error);
             return NextResponse.json(
               { error: "⚠️ Oops! Failed to retrieve the todos. Please try again." },
+              { status: 500 }
+            );
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({ error: "🚫 Unauthorized. Please log in to retrieve the todos." }, { status: 401 });
+        }
+        const userId = session?.user.id;
+        //const userId = "67e051d767ef5cc8fa0a5f83";
+        //const { title, description } = await request.json();
+        
+        const body: ITodo = await request.json();
+        if (!body.title || !body.description) {
+            return NextResponse.json({ error: "😠 Please enter a title or description." }, { status: 401 });
+        }
+        //const newTodo = await Todo.create({title, description, userId: userId});
+        await connectToDatabase();
+        const newTodo= await Todo.create({ ...body, userId: userId });
+        return NextResponse.json({ message:"✅ Successfully created the new todo.",todo:newTodo }, { status: 201 });
+        
+    } catch (error) {
+        console.error("❌ Error creating the new todo:", error);
+            return NextResponse.json(
+              { error: "⚠️ Oops! Failed to create the new todo. Please try again." },
               { status: 500 }
             );
     }

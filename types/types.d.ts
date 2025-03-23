b86abf58aacbc2cf14332863b6bb7bef80588548
Sowparna Shome <soupornoshome@gmail.com>
2025-03-23 12:25:37 +0530

    mongoose type and db connection create

---
+++ types/types.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-var */
+import { Connection } from "mongoose"
+
+declare global{
+    var mongoose: {
+        conn: Connection | null;
+        promise: Promise<Connection> | null;
+    };
+}
+export {}

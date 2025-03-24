43b9d36968c40cd308dc607351eb142d14414c7e
Sowparna Shome <soupornoshome@gmail.com>
2025-03-24 23:03:21 +0530

    todo view add

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import TodoView from "@/components/todos/TodoView";
+
 export default function Home() {
   return (
-    <><h1 className="text-3xl text-center mt-5">😊😊😊track your tasks!!!</h1></>
+    <><h1 className="text-3xl text-center mt-5">😊😊😊track your tasks!!!</h1>
+    <TodoView/>
+    </>
   );
 }

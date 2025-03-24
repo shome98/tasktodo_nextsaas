5e43ce27bee4dfe3c02cc39e303d570e9903a17a
Shome234 <158542025+Shome234@users.noreply.github.com>
2025-03-24 07:41:04 +0000

    update todo

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -38,3 +38,31 @@ export async function GET(request:NextRequest,props: { params: Promise<{ id: str
         );
     }
 }
+
+export async function PUT(request:NextRequest,props: { params: Promise<{ id: string }> }){
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            return NextResponse.json({ error: "🚫 Unauthorized. Please log in to retrieve the todo." }, { status: 401 });
+        }
+        const userId = session?.user.id;
+        const {id}=await props.params;
+        const body:ITodo=await request.json();
+        if(!id){
+            return NextResponse.json({ error: "🚫 Not a valid param!" }, { status: 401 });
+        }
+        await connectToDatabase();
+        const updatedTodo=await Todo.findOneAndUpdate({_id:id,userId:userId},{...body},{new:true}).lean();
+
+        if(!updatedTodo){
+            return NextResponse.json({error:"🚫Could not update the todo. Please try again",},{status:401});
+        }
+        NextResponse.json({message:"😊Successfully updated the todo.",todo:updatedTodo},{status:201});
+    } catch (error) {
+        console.error("❌ Error updating the todo:", error);
+        return NextResponse.json(
+          { error: "⚠️ Oops! Failed to update the todo. Please try again." },
+          { status: 500 }
+        );
+    }
+}

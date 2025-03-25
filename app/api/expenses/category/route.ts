bb805bdd8ad4502347b60f4646093e53a3484fea
Shome234 <158542025+Shome234@users.noreply.github.com>
2025-03-25 07:42:18 +0000

    add new category

--- app/api/expenses/category/route.ts
+++ app/api/expenses/category/route.ts
@@ -27,3 +27,29 @@ export async function GET(request:NextRequest){
     }
 }
 
+export async function POST(request:NextRequest){
+    try {
+        const session = await getServerSession(authOptions);
+                if (!session) {
+                    return NextResponse.json({ error: "🚫 Unauthorized. Please log in to add categories." }, { status: 401 });
+                }
+                const userId = session?.user.id;
+                const body:ICategory=await request.json();
+                if(body.names.length===0){
+                    return NextResponse.json({ error: "😠 Please enter atleast one category" }, { status: 401 });
+                }
+                await connectToDatabase();
+                const newcategories=await Category.create({names:body.names,userId:userId});
+                if(!newcategories){
+                    return NextResponse.json({ error: "🚫 Failed to add the categories." }, { status: 401 });
+                }
+                return NextResponse.json({ message:"✅ Successfully added the new categories.",categories: newcategories }, { status: 201 });
+
+    } catch (error) {
+        console.error("❌ Error addining the categories:", error);
+            return NextResponse.json(
+              { error: "⚠️ Oops! Failed to add the categories. Please try again." },
+              { status: 500 }
+            );
+    }
+}

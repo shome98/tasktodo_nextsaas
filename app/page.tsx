d11931edf2cc75b10b50e6127bce3c08bb454164
Sowparna Shome <soupornoshome@gmail.com>
2025-03-25 23:43:39 +0530

    homeviewone add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 //import TodoView from "@/components/todos/TodoView";
-import TodoViewTable from "@/components/todos/TodoViewTable";
+//import TodoViewTable from "@/components/todos/TodoViewTable";
+
+import HomeViewOne from "@/components/HomeViewOne";
 
 export default function Home() {
   return (
     <>
-    <TodoViewTable/>
+      {/* <TodoViewTable/> */}
+      <HomeViewOne/>
     </>
   );
 }

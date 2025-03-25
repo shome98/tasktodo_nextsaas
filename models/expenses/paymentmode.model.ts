21bc26065a26ae81e0f3dc82aee96a489c439986
Shome234 <158542025+Shome234@users.noreply.github.com>
2025-03-25 06:49:38 +0000

    payment modes store as array of names

---
+++ models/expenses/paymentmode.model.ts
@@ -0,0 +1,22 @@
+import mongoose,{ Document, models, Schema } from "mongoose";
+
+// export interface IPaymentMode extends Document{
+//     name:string;
+//     userId:string|Schema.Types.ObjectId;
+//     createdAt?:Date;
+//     updatedAt?:Date;
+// }
+export interface IPaymentMode extends Document{
+    names:string[];
+    userId:string|Schema.Types.ObjectId;
+    createdAt?:Date;
+    updatedAt?:Date;
+}
+
+const paymentModeSchema=new Schema<IPaymentMode>({
+    names:{type:[String],required:true},
+    userId:{type:Schema.Types.ObjectId,ref:"User",required:true}
+},{timestamps:true});
+
+const PaymentMode=models?.PaymentMode||mongoose.model<IPaymentMode>("PaymentMode",paymentModeSchema);
+export default PaymentMode;

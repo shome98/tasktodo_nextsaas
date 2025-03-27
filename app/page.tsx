e834febdd15c4c4b77f155298d2eb232bca6b52e
Sowparna Shome <soupornoshome@gmail.com>
2025-03-27 23:29:59 +0530

    remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
-//import TodoView from "@/components/todos/TodoView";
-// import TodoViewTable from "@/components/todos/TodoViewTable";
-
-// import HomeViewTwo from "@/components/HomeViewTwo";
 import TodoViewv1 from "@/components/views/TodoViewv1";
 import PaymentModeViewv1 from "@/components/views/PaymentModeViewv1";
 import CategoryViewv1 from "@/components/views/CategoryViewv1";
 import Expenseviewv1 from "@/components/views/ExpenseViewv1";
 
-//import HomeViewOne from "@/components/HomeViewOne";
-
 export default function Home() {
   return (
     <>
-      {/* <TodoViewTable/> */}
-      {/* <HomeViewOne/> */}
-      {/* <HomeViewTwo/> */}
       <TodoViewv1 />
       <PaymentModeViewv1 />
       <CategoryViewv1 />

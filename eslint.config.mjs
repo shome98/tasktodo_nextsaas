b47fbe7973c948aebc4e59a06b50e8766b8de847
shome98 <soupornoshome@gmail.com>
2025-03-27 14:13:35 +0530

    rules remove

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    "rules":{
-      "no-unused-vars":["error",{"varsIgnorePattern":"^(Category|PaymentMode)$"}],
-    }
-  }
 ];
 
 export default eslintConfig;

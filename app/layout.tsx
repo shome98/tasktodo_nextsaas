32548a89c9f632955e9a431a55f9461c295dabe8
Sowparna Shome <soupornoshome@gmail.com>
2025-03-29 23:13:07 +0530

    provider wrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Montserrat, Nunito_Sans } from "next/font/google";
 import "./globals.css";
-import AuthProvider from "@/components/AuthProvider";
 import { Toaster } from "sonner";
 import { NavBar } from "@/components/nav/NavBar";
 import Footer from "@/components/Footer";
+import Providers from "@/components/Providers";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -29,18 +29,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthProvider>
-    <html lang="en" className="dark">
-      <body
-        className={`${montserrat.variable} ${nunitoSans.variable} antialiased`}
-      ><div className="flex flex-col min-h-screen">
-    <NavBar />
-    <main className="flex-1 pt-17">{children}</main>
-    <Footer />
-  </div>
+    
+      <html lang="en" className="dark">
+      <body className={`${montserrat.variable} ${nunitoSans.variable} antialiased`}>
+        <Providers>
+          <div className="flex flex-col min-h-screen">
+            <NavBar />
+            <main className="flex-1 pt-17">{children}</main>
+            <Footer />
+          </div>
           <Toaster />
-      </body>
+          </Providers>
+        </body>
       </html>
-      </AuthProvider>
+      
   );
 }
